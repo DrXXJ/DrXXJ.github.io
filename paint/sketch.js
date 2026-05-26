772d00b8db463e694c4494c16380712d71a808bd
DrXXJ <taikogo3150@gmail.com>
2026-05-26 14:00:07 +0900

    一旦

--- paint/sketch.js
+++ paint/sketch.js
@@ -1,20 +1,84 @@
 // Paint
 
+let px;
+let py;
+
+let sw = 1;
+
+let inputColor;
+let inputWeight;
+let btnClear;
+let btnSave;
+
 //最初の処理
 function setup() {
- createCanvas(800, 800)
+ createCanvas(600, 500)
+ background(255)
+
+ pixelDensity(1);
+
+ let code = getItem('paint')
+ decodePixels(code);
+ inputColor = select('#color');
+ inputWeight = select('#weight');
+
+ btnClear = select('#clear');
+ btnClear.mousePressed(clearAll);
+
+ btnSave = select('#save');
+ btnSave.mousePressed(saveImg);
+
+ update();
+
 }
 
 //フレーム前の処理
 function draw() {
   //background(220);
 
-  noStroke(); //境界線を消す
-  fill(0, 0, 0);//塗りの色
+
+  if ( keyIsPressed ) {
+    if ( key =='f' ) {
+      sw += 2;
+
+    }else if ( key == 'd' ) {
+      sw -= 2;
+
+    }
+  }
+
+  sw = constrain(sw, 1, 100);
+
+  stroke(inputColor.value());
+  strokeWeight(sw);
 
   if( mouseIsPressed ){
     //条件が trueだったら実行
-     circle(mouseX, mouseY, 80);
+     line(px, py, mouseX, mouseY)
+     noStroke();
+     circle(mouseX, mouseY, sw * random(0.9, 1.1));
+   
   }
+  update();
 }
 
+
+function update(){
+  px = mouseX;
+  py = mouseY;
+}
+
+function clearAll(){
+  background(255);
+}
+
+function saveImg(){
+   saveCanvas('paint.png');
+}
+
+function mouseReleased() {
+  console.log('マウスを放しました。 ');
+  let code = encodePixels();
+  console.log(code);
+  storeItem('paint', code);
+}
--- qrgen/sketch.js
+++ qrgen/sketch.js
@@ -1,20 +1,20 @@
- let qrData;
- let urlInput;
- let input; // 
+let qrData;
+let input; // 
 let colorInput;
 let bgInput;
 
 
 function setup() {
   createCanvas(600, 600);
-  frameRate(15);
+  // frameRate(15);
   input = select('#url'); // HTMLタグを取得する（id は url）
-	colorInput = select('#color');
-	bgInput = select('#bg');
+  colorInput = select('#color');
+  bgInput = select('#bg');
 }
 
 function draw() {
-  background(bgInput.value());
+  // background(bgInput.value());
+  background(220);
 
   textSize(20);
 
@@ -22,23 +22,19 @@ function draw() {
 
   qrData = qr.encodeQR(input.value(), 'raw');
 
-noStroke();
-	fill(colorInput.value());
-
-
+  noStroke();
+  fill(colorInput.value());
 
   for (let y = 0; y < qrData.length; y++) {
-    let row = qrData[y];
-    for (let x = 0; x < row.length; x++) {
-      let cell = row[x];
+    let line = qrData[y];
+    for (let x = 0; x < line.length; x++) {
+      let cell = line[x];
       if (cell == true) {
         circle(x * gap, y * gap, 20);
         // text(ceil(random(0, 9)), x * gap, y * gap);
       }
     }
   }
-
-  text(urlInput.value(), 200, 200);
 }
 
 
